bd407d626ea34d4f6c694ae33f114bf4ac4d0de7
DESKTOP-GE4BND5\Jano <janek.zar12@gmail.com>
2026-04-24 12:30:36 +0200

    no message

--- DotNet/MvcMovie/obj/Debug/net8.0/MvcMovie.AssemblyInfo.cs
+++ DotNet/MvcMovie/obj/Debug/net8.0/MvcMovie.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MvcMovie")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6bcf767df139b0a4313c64603ad97ee53ae79386")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f264b54f7566f2b4d8e8bbb3542ed672478b597a")]
 [assembly: System.Reflection.AssemblyProductAttribute("MvcMovie")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MvcMovie")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
